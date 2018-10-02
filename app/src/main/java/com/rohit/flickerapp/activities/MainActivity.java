ba6610b8017e190184c4da43d4cda06cc930f159
Rohit Kumar <rohit9075@gmail.com>
2018-10-02 13:33:41 +0530

    Adding the project Notes

--- app/src/main/java/com/rohit/flickerapp/activities/MainActivity.java
+++ app/src/main/java/com/rohit/flickerapp/activities/MainActivity.java
@@ -172,7 +172,7 @@ public class MainActivity extends AppCompatActivity implements SwipeRefreshLayou
             if(mSwipeRefreshLayout.isRefreshing()){
                 mSwipeRefreshLayout.setRefreshing(false);
             }
-            recyclerViewInitialisation();
+            recyclerViewInitialisation(); // method call
 
 
         }
