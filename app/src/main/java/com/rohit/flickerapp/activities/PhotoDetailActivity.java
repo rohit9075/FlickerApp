cae72d45900c4ad93f964d93f779665cdc0511c9
Rohit Kumar <rohit9075@gmail.com>
2018-10-09 10:58:19 +0530

    java and xml code

--- app/src/main/java/com/rohit/flickerapp/activities/PhotoDetailActivity.java
+++ app/src/main/java/com/rohit/flickerapp/activities/PhotoDetailActivity.java
@@ -1,18 +1,58 @@
 package com.rohit.flickerapp.activities;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Toast;
+import android.os.StrictMode;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.rohit.flickerapp.R;
+import com.rohit.flickerapp.utils.SharedPreference;
+import com.squareup.picasso.Picasso;
 
-public class PhotoDetailActivity extends AppCompatActivity {
+public class PhotoDetailActivity extends AppCompatActivity  {
+
+    private TextView author,title,tags,link;
+    private ImageView image;
+    private SharedPreference sharedPreference;
+
+
+    private String[] listItems;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_photo_detail);
 
-        Toast.makeText(this, "Welcome you to photo Detail Activity", Toast.LENGTH_SHORT).show();
+        initiateViews();
+
+        listItems = new String[]{"Download Image", "Share Image","Share Image Link"};
+
+        StrictMode.VmPolicy.Builder builder = new StrictMode.VmPolicy.Builder(); StrictMode.setVmPolicy(builder.build());
+
+        sharedPreference = new SharedPreference(this);
+
+        author.setText("Author : " + sharedPreference.getImageAuthor());
+        title.setText("Title : "+sharedPreference.getImageTitle());
+        tags.setText("Tags : "+sharedPreference.getImageTags());
+        link.setText("Link : "+sharedPreference.getUrl());
+        Picasso.with(PhotoDetailActivity.this).load(sharedPreference.getUrl()).into(image);
+
     }
+
+    public void initiateViews(){
+
+        author = (TextView)findViewById(R.id.photo_author);
+        title = (TextView)findViewById(R.id.photo_title);
+        tags = (TextView)findViewById(R.id.photo_tags);
+        link = (TextView)findViewById(R.id.photo_link);
+
+        image = (ImageView)findViewById(R.id.photo_image);
+
+    }
+
+
+
+
 }
